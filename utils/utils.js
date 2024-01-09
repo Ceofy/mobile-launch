9780d682c0422475053838de4097ef5865528c30
ceofy <sol015@ucsd.edu>
2024-01-08 21:42:31 -0700

    Got automatic login working on android

--- utils/utils.js
+++ utils/utils.js
@@ -1,13 +1,28 @@
 import * as SecureStore from 'expo-secure-store';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const saveToSecureStore = async (key, value) => {
-  if (value == null) {
+export const saveToSecureStorage = async (key, value) => {
+  // False, null, or empty string
+  if (!value && value !== 0) {
     value = '';
   }
-  await SecureStore.setItemAsync(key, value);
+  await SecureStore.setItemAsync(key, value.toString());
 };
 
-export const getValueFromSecureStore = async key => {
+export const getValueFromSecureStorage = async key => {
   let result = await SecureStore.getItemAsync(key);
+  return result;
+};
+
+export const saveToStorage = async (key, value) => {
+  // False, null, or empty string
+  if (!value && value !== 0) {
+    value = '';
+  }
+  await AsyncStorage.setItem(key, value.toString());
+};
+
+export const getValueFromStorage = async key => {
+  let result = await AsyncStorage.getItem(key);
   return result;
 };
